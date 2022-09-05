98901130df69993d17654501ce1705f5edb1d36b
dgcezar <diego.cezar13@gmail.com>
2022-09-05 11:59:09 -0300

    Primeiro - Exercício 1 concluído

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const sacolao = [];
+
+const frutas = {
+  nome: ["goiaba", "laranja", "mamao"],
+  preco: [2, 4, 7],
+  disponibilidade: [true, true, true],
+};
+
+sacolao.push(frutas);
+
+console.log(sacolao);
